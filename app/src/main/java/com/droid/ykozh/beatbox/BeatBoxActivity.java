9df1cc13c6a423fe001291b0d359412c886f6043
Yaroslav Kozhumyaka <y.kozhumyaka@gmail.com>
2018-04-07 13:52:55 +0300

    added Sound Pull

---
+++ app/src/main/java/com/droid/ykozh/beatbox/BeatBoxActivity.java
@@ -0,0 +1,12 @@
+package com.droid.ykozh.beatbox;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/com/droid/ykozh/beatbox/Sound.java
@@ -0,0 +1,30 @@
+package com.droid.ykozh.beatbox;
+
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public void setAssetPath(String assetPath) {
+        mAssetPath = assetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
